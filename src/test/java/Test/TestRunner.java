ad9ae76f8a7ba4c3fd252b74f14810dde10fe435
sreenivas735 <76997569+sreenivas735@users.noreply.github.com>
2024-01-26 20:14:40 +0530

    Update TestRunner.java

--- src/test/java/Test/TestRunner.java
+++ src/test/java/Test/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src\\test\\resources\\Orange.feature",
+        features = "src/test/resources/Orange.feature",
         glue = "Test",
         tags = "@loginpage",
         plugin = {"pretty", "html:target/orangehrm.html"}
