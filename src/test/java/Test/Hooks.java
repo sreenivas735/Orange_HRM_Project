0762f3226c407c769d41c4ca869ad8ccd2a064bf
sreenivas <vasusurepalli735@gmail.com>
2024-01-28 10:29:56 +0530

    test

--- src/test/java/Test/Hooks.java
+++ src/test/java/Test/Hooks.java
@@ -2,6 +2,7 @@ package Test;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -27,12 +28,16 @@ public class Hooks {
         String browser = prop.getProperty("browser");
         String app_url = prop.getProperty("application_url");
 
+
+
+
+
         if (browser.equals("edge")) {
             System.setProperty("webdriver.edge.driver", path + "/msedgedriver.exe");
             driver = new EdgeDriver();
 
         } else if (browser.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "path + \"/msedgedriver.exe");
             driver = new ChromeDriver();
         } else {
             throw new InvalidObjectException("driver not found");
